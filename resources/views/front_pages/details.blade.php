bb48eac7853048952b57586c3e6151536cd4a5c1
chandan <chandan84yadav@gmail.com>
2024-07-31 00:04:16 +0530

    changes

--- resources/views/front_pages/details.blade.php
+++ resources/views/front_pages/details.blade.php
@@ -72,20 +72,16 @@
                     <div class="col-lg-6">
                         <h5 class="section-booking-title pe-3">Booking</h5>
                         <h1 class="text-white mb-4">Online Booking</h1>
-                        <p class="text-white mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-                            maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus
-                            praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
+                        <p class="text-white mb-4">Experience seamless travel planning with AC Vacations' Online Reservation Start. Our user-friendly platform allows you to effortlessly browse and book your ideal vacation package from the comfort of your home.
                         </p>
-                        <p class="text-white mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-                            maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus
-                            praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
+                        <p class="text-white mb-4">With real-time availability, secure transactions, and instant confirmations, planning your next adventure has never been easier. Begin your journey today with just a few clicks and let us handle the rest for a hassle-free holiday experience.
                         </p>
-                        <a href="#" class="btn btn-light text-primary rounded-pill py-3 px-5 mt-2">Read More</a>
+                        {{--                    <a href="#" class="btn btn-light text-primary rounded-pill py-3 px-5 mt-2">Read More</a>--}}
                     </div>
                     <div class="col-lg-6">
                         <h1 class="text-white mb-3">Book A Tour Deals</h1>
-                        <p class="text-white mb-4">Get <span class="text-warning">50% Off</span> On Your First Adventure
-                            Trip With. Get More Deal Offers Here.</p>
+                        {{--                    <p class="text-white mb-4">Get <span class="text-warning">50% Off</span> On Your First Adventure--}}
+                        {{--                        Trip With. Get More Deal Offers Here.</p>--}}
                         <form method="post" action="{{route('bookTable.store')}}">
                             @csrf
                             <div class="row g-3">
@@ -103,45 +99,51 @@
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-floating date" id="date3" data-target-input="nearest">
-                                        <input type="text" class="form-control bg-white border-0" id="datetime" name="date_time" placeholder="Date & Time" data-target="#date3" data-toggle="datetimepicker" />
+                                        <input type="datetime-local" class="form-control bg-white border-0" id="datetime" name="date_time" placeholder="Date & Time" data-target="#date3" data-toggle="datetimepicker" />
                                         <label for="datetime">Date & Time</label>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-floating">
-                                        <select class="form-select bg-white border-0" id="select1" name="destination">
-                                            <option value="Destination 1">Destination 1</option>
-                                            <option value="Destination 2">Destination 2</option>
-                                            <option value="Destination 3">Destination 3</option>
-                                        </select>
+                                        <input type="text" class="form-control bg-white border-0" id="destination" name="destination" placeholder="Your destination Name">
+
+                                        {{--                                    <select class="form-select bg-white border-0" id="select1" name="destination">--}}
+                                        {{--                                        <option value="Destination 1">Destination 1</option>--}}
+                                        {{--                                        <option value="Destination 2">Destination 2</option>--}}
+                                        {{--                                        <option value="Destination 3">Destination 3</option>--}}
+                                        {{--                                    </select>--}}
                                         <label for="select1">Destination</label>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-floating">
-                                        <select class="form-select bg-white border-0" id="SelectPerson" name="person">
-                                            <option value="1">Persons 1</option>
-                                            <option value="2">Persons 2</option>
-                                            <option value="3">Persons 3</option>
-                                        </select>
-                                        <label for="SelectPerson">Persons</label>
+                                        <input type="text" class="form-control bg-white border-0" id="person" name="person" placeholder="persons">
+
+                                        {{--                                    <select class="form-select bg-white border-0" id="SelectPerson" name="person">--}}
+                                        {{--                                        <option value="1">Persons 1</option>--}}
+                                        {{--                                        <option value="2">Persons 2</option>--}}
+                                        {{--                                        <option value="3">Persons 3</option>--}}
+                                        {{--                                    </select>--}}
+                                        <label for="SelectPerson">Number of Guests</label>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-floating">
-                                        <select class="form-select bg-white border-0" id="CategoriesSelect" name="categories">
-                                            <option value="Kids">Kids</option>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                        </select>
-                                        <label for="CategoriesSelect">Categories</label>
+                                        <input type="text" class="form-control bg-white border-0" id="number" name="number" placeholder="Phone Number">
+
+                                        {{--                                    <select class="form-select bg-white border-0" id="CategoriesSelect" name="categories">--}}
+                                        {{--                                        <option value="Kids">Kids</option>--}}
+                                        {{--                                        <option value="1">1</option>--}}
+                                        {{--                                        <option value="2">2</option>--}}
+                                        {{--                                        <option value="3">3</option>--}}
+                                        {{--                                    </select>--}}
+                                        <label for="CategoriesSelect">Phone Number</label>
                                     </div>
                                 </div>
                                 <div class="col-12">
                                     <div class="form-floating">
                                         <textarea class="form-control bg-white border-0" placeholder="Special Request" id="message" name="msg" style="height: 100px"></textarea>
-                                        <label for="message">Special Request</label>
+                                        <label for="message">Messsage</label>
                                     </div>
                                 </div>
                                 <div class="col-12">
