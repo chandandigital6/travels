cee9ba3df235251f45d496143714fa3f3989ada8
chandan <chandan84yadav@gmail.com>
2024-08-01 13:38:44 +0530

    fix layout

--- resources/views/front_pages/about.blade.php
+++ resources/views/front_pages/about.blade.php
@@ -5,7 +5,7 @@
         <div class="container text-center py-5" style="max-width: 900px;">
             <h3 class="text-white display-3 mb-4">About Us</h3>
                 <ol class="breadcrumb justify-content-center mb-0">
-                    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                    <li class="breadcrumb-item "><a href="{{route('home')}}" >Home</a></li>
 {{--                    <li class="breadcrumb-item"><a href="#">Pages</a></li>--}}
                     <li class="breadcrumb-item active text-white">About</li>
                 </ol>
